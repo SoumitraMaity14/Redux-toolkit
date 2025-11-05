5d119cae83009a4e86a6f627181d14d013b35600
SOumitra Maity <maitychottu03225@gmail.com>
2025-11-05 09:04:22 +0530

    new changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store');
 const  cakeActions =require('./features/cake/cakeSlice').cakeActions
+const icecreamActions =require('./features/icecream/icecreamSlice').icecreamActions
 
 console.log('intial state', store.getState())
 
@@ -12,6 +13,10 @@ store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 
 store.dispatch(cakeActions.restocked(3))
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
 
 unsubscribe()
 
