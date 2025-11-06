e534905aaa5a6207dc2bd5907e329bacb35aaf2c
SOumitra Maity <maitychottu03225@gmail.com>
2025-11-06 08:24:38 +0530

    new changes

--- app/store.js
+++ app/store.js
@@ -2,13 +2,13 @@ const cakeReducer= require('../features/cake/cakeSlice');
 const icecreamReducer =require('../features/icecream/icecreamSlice');
 const reduxLogger = require('redux-logger');
 const configureStore= require('@reduxjs/toolkit').configureStore;
-const logger = reduxLogger.createLogger();
+// const logger = reduxLogger.createLogger();
 const store= configureStore({
     reducer:{
         cake:cakeReducer,
         icecream: icecreamReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 module.exports=store;
